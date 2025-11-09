d4aad674144b4463d976a11e1b159b44308ff16c
IvinoDev <kayantao@protonmail.com>
2025-11-09 18:07:56 +0000

    comment format

--- developementmobile/activite1/lib/modele/redacteur.dart
+++ developementmobile/activite1/lib/modele/redacteur.dart
@@ -4,7 +4,7 @@ class Redacteur {
   String prenom;
   String email;
 
-  // Constructeur avec id
+  //* Constructeur avec id
   Redacteur({
     this.id,
     required this.nom,
@@ -12,19 +12,19 @@ class Redacteur {
     required this.email,
   });
 
-  // Constructeur sans id
+  //* Constructeur sans id
   Redacteur.sansId({
     required this.nom,
     required this.prenom,
     required this.email,
   });
 
-  // Convertir un Redacteur en Map
+  //* Convertir un Redacteur en Map
   Map<String, dynamic> toMap() {
     return {'id': id, 'nom': nom, 'prenom': prenom, 'email': email};
   }
 
-  // Créer un Redacteur à partir d'un Map
+  //* Créer un Redacteur à partir d'un Map
   factory Redacteur.fromMap(Map<String, dynamic> map) {
     return Redacteur(
       id: map['id'],
